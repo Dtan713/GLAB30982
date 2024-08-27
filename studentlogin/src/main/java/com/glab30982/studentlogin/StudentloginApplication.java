2c1526e1055d6107fdf6341673f91f62f4ec7ae7
Dtan713 <s.donaldtan@gmail.com>
2024-08-27 09:21:26 -0400

    adding more

--- studentlogin/src/main/java/com/glab30982/studentlogin/StudentloginApplication.java
+++ studentlogin/src/main/java/com/glab30982/studentlogin/StudentloginApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentloginApplication {
+public class StudentLoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentloginApplication.class, args);
---
+++ studentlogin/src/main/java/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long>
+{
+
+    Student findByEmail(String email);
+}
